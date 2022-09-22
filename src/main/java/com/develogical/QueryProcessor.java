0ff73cc089d36ef4ddc41281fde3bb0c07048d5a
Nelly Hadlaw <nellyhadlaw@Nellys-MacBook-Pro.local>
2022-09-22 15:26:38 -0400

    added semi colon

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("your name")) {
-            return "nelzer101"
+            return "nelzer101";
         }
         return "";
     }
